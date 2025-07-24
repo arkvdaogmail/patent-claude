f3ab26508aaafc6b4b50fe96ddd3770726f1a9c7
arkvdaogmail <arkvdao@gmail.com>
2025-07-23 18:19:07 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import MainUI from './components/MainUI';
+
 export default function App() {
   return (
     <div>
-      <h1>Hello Vite + React!</h1>
-      <img src="/vite.svg" alt="Vite logo" style={{ width: 120 }} />
-      <p>If you see this, your React/Vite app is rendering at the most basic level.</p>
+      <MainUI />
     </div>
   );
 }
